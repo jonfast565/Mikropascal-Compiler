45bc8906c48c49b4e1bb297db7d6affc47238da8
jonfast565 <jnfstdj656@gmail.com>
2014-02-07 12:58:31 -0700

    More parser code...
    Some more parser code stuff…

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -12,25 +12,22 @@ Parser::Parser(shared_ptr<vector<shared_ptr<Token>>> token_list) {
 	this->fromList = true;
 }
 
+// USE ONLY THIS CONSTRUCTOR FOR NOW!!!!
 Parser::Parser(shared_ptr<Scanner> scanner) {
 	this->scanner = scanner;
 	this->fromList = false;
 }
 
-void Parser::parseMe() {
-	if (this->fromList == false) {
-		// parse by calling the scanner's dispatcher
-	}
-	else if (this->fromList == true) {
-		// parse by pulling off a token from the beginning
-		// of the list and attempting to parse it
-	}
+bool Parser::just_match(TokType expected) {
+	if (this->lookahead != expected)
+		return false;
+	else return true;
 }
 
 void Parser::match(TokType expected) {
 	if (this->lookahead != expected) {
-		cout << "Syntax error, expected " << get_token_info(expected).first <<
-				" but got " << get_token_info(this->lookahead->get_token()).first << " instead. Fail!" << endl;
+		report_error(string("Syntax error, expected " + get_token_info(expected).first +
+				" but got " + get_token_info(this->lookahead->get_token()).first + " instead. Fail!"));
 	} else {
 		// consume the token and get the next
 		if (this->fromList == false) {
@@ -40,6 +37,413 @@ void Parser::match(TokType expected) {
 		else if (this->fromList == true) {
 			// implement this later... with detaching token list
 		}
+	}
+}
+
+void Parser::parseMe() {
+	// put the next token in the global buffer
+	// parse the system goal!
+	this->next_token();
+	this->parse_system_goal();
+}
+
+void Parser::parse_system_goal() {
+	// parse system goal
+	this->parse_program();
+	this->parse_eof();
+}
+
+void Parser::parse_eof() {
+	bool is_eof = this->just_match(MP_EOF);
+	if (!is_eof) {
+		report_error(string("No end-of-file detected."));
+	}
+}
+
+void Parser::parse_program() {
+	this->parse_program_heading();
+	this->match(MP_SEMI_COLON);
+	this->parse_block();
+	this->match(MP_PERIOD);
+}
+
+void Parser::parse_variable_declaration_part() {
+	// is an identifier clause
+	if (this->just_match(MP_VAR)) {
+		this->parse_variable_declaration();
+		this->match(MP_SEMI_COLON);
+		this->parse_variable_declaration_tail();
+	}
+	else {
+		// or matches epsilon
+	}
+}
+
+void Parser::parse_variable_declaration_tail() {
+	if (this->just_match(MP_VAR)) {
+		this->parse_variable_declaration();
+		this->match(MP_SEMI_COLON);
+		// recursive so watch out!
+		this->parse_variable_declaration_tail();
+	}
+	else {
+		// or matches epsilon
+	}
+}
+
+void Parser::parse_variable_declaration() {
+	this->parse_identifier_list();
+	this->match(MP_COLON);
+	this->parse_type();
+}
+
+void Parser::parse_type() {
+	bool integer_match = just_match(MP_INTEGER);
+	bool float_match = just_match(MP_FLOAT);
+	bool string_match = just_match(MP_STRING);
+	bool boolean_match = just_match(MP_BOOLEAN);
+	if (integer_match) {
+		this->report_match("Matched integer decl.");
+	}
+	else if (float_match) {
+		this->report_match("Matched float decl.");
+	}
+	else if (string_match) {
+		this->report_match("Matched string decl.");
+	}
+	else if (boolean_match) {
+		this->report_match("Matched boolean decl.");
+	}
+	else {
+		this->report_error(string("Syntax is incorrect when matching identifier."));
+	}
+}
+
+void Parser::parse_procedure_and_function_declaration_part() {
+	if (this->just_match(MP_PROCEDURE)) {
+		this->parse_procedure_declaration();
+		this->parse_procedure_and_function_declaration_part();
+	} else if (this->just_match(MP_FUNCTION)) {
+		this->parse_function_declaration();
+		this->parse_procedure_and_function_declaration_part();
+	} else {
+		// or epsilon
+	}
+}
+
+void Parser::parse_procedure_declaration() {
+	this->parse_procedure_heading();
+	this->match(MP_SEMI_COLON);
+	this->parse_block();
+	this->match(MP_SEMI_COLON);
+}
+
+void Parser::parse_function_declaration() {
+	this->parse_function_declaration();
+	this->match(MP_SEMI_COLON);
+	this->parse_block();
+	this->match(MP_SEMI_COLON);
+}
+
+void Parser::parse_procedure_heading() {
+	this->match(MP_PROCEDURE);
+	this->parse_procedure_identifier();
+	this->parse_optional_formal_parameter_list();
+	this->parse_type();
+}
+
+void Parser::parse_function_heading() {
+	this->match(MP_FUNCTION);
+	this->parse_function_identifier();
+	this->parse_optional_formal_parameter_list();
+	this->parse_type();
+}
+
+void Parser::parse_optional_formal_parameter_list() {
+	if (this->just_match(MP_LEFT_PAREN)) {
+		this->match(MP_LEFT_PAREN);
+		this->parse_formal_parameter_section();
+		this->parse_formal_parameter_section_tail();
+		this->match(MP_RIGHT_PAREN);
+	}
+	else {
+		// or match epsilon
+	}
+}
+
+void Parser::parse_formal_parameter_section_tail() {
+	if (this->just_match(MP_SEMI_COLON)) {
+		this->match(MP_SEMI_COLON);
+		this->parse_formal_parameter_section();
+		this->parse_formal_parameter_section_tail();
+	} else {
+		// or match epsilon
+	}
+}
+
+void Parser::parse_formal_parameter_section() {
+	if (this->just_match(MP_ID)) {
+		this->parse_value_parameter_section();
+	} else if (this->just_match(MP_VAR)) {
+		this->parse_variable_parameter_section();
+	}
+}
+
+void Parser::parse_value_parameter_section() {
+	this->parse_identifier_list();
+	this->match(MP_COLON);
+	this->parse_type();
+}
+
+void Parser::parse_variable_parameter_section() {
+	this->match(MP_VAR);
+	this->parse_identifier_list();
+	this->match(MP_COLON);
+	this->parse_type();
+}
+
+void Parser::parse_statement_part() {
+	this->parse_compound_statement();
+}
+
+void Parser::parse_compound_statement() {
+	this->match(MP_BEGIN);
+	this->parse_statement_sequence();
+	this->match(MP_END);
+}
+
+void Parser::parse_statement_sequence() {
+	this->parse_statement();
+	this->parse_statement_tail();
+}
+
+void Parser::parse_statement_tail() {
+	if (this->just_match(MP_SEMI_COLON)) {
+		this->match(MP_SEMI_COLON);
+		this->parse_statement();
+		this->parse_statement_tail();
+	} else {
+		// or match epsilon
+	}
+}
+
+void Parser::parse_statement() {
+	this->parse_compound_statement();
+	this->parse_read_statement();
+	this->parse_write_statement();
+	this->parse_assignment_statement();
+	this->parse_if_statement();
+	this->parse_while_statement();
+	this->parse_repeat_statement();
+	this->parse_for_statement();
+	this->parse_procedure_statement();
+}
+
+void Parser::parse_empty_statement() {
+	// ? epsilon here
+}
+
+void Parser::parse_read_statement() {
+	this->match(MP_READ);
+	this->match(MP_LEFT_PAREN);
+	this->parse_read_parameter();
+	this->parse_read_parameter_tail();
+	this->match(MP_RIGHT_PAREN);
+}
+
+void Parser::parse_read_parameter_tail() {
+	if (this->just_match(MP_COMMA)) {
+		this->parse_read_parameter();
+		this->parse_read_parameter_tail();
+	} else {
+		// matched epsilon
+	}
+}
+
+void Parser::parse_read_parameter() {
+	this->parse_variable_identifier();
+}
+
+void Parser::parse_write_statement() {
+	if (this->just_match(MP_WRITELN)) {
+		this->match(MP_WRITELN);
+	} else if (this->just_match(MP_WRITE)) {
+		this->match(MP_WRITE);
+	}
+	this->match(MP_LEFT_PAREN);
+	this->parse_write_parameter();
+	this->parse_write_parameter_tail();
+	this->match(MP_RIGHT_PAREN);
+}
+
+void Parser::parse_write_parameter_tail() {
+	if (this->just_match(MP_COMMA)) {
+		this->match(MP_COMMA);
+		this->parse_write_parameter();
+		this->parse_write_parameter_tail();
+	} else {
+		// matched epsilon
+	}
+}
+
+void Parser::parse_write_parameter() {
+	this->parse_ordinal_expression();
+}
+
+void Parser::parse_assignment_statement() {
+	// do something clever here...
+	// it won't work otherwise
+	this->parse_variable_identifier();
+	this->match(MP_ASSIGNMENT);
+	this->parse_expression();
+
+	// or...
+	this->parse_variable_identifier();
+	this->match(MP_ASSIGNMENT);
+	this->parse_expression();
+}
+
+void Parser::parse_if_statement() {
+	if (this->just_match(MP_IF)) {
+		this->match(MP_IF);
+		this->parse_boolean_expression();
+		this->match(MP_THEN);
+		this->parse_statement();
+		this->parse_optional_else_part();
+	} else {
+		// report not the statement
+		// we were looking for
+	}
+}
+
+void Parser::parse_optional_else_part() {
+	if (this->just_match(MP_ELSE)) {
+		// we have an else part
+		this->match(MP_ELSE);
+		this->parse_statement();
+	} else {
+		// or epsilon
+	}
+}
+
+void Parser::parse_repeat_statement() {
+	if (this->just_match(MP_REPEAT)) {
+		this->match(MP_REPEAT);
+		this->parse_statement_sequence();
+		this->match(MP_UNTIL);
+		this->parse_boolean_expression();
+	} else {
+		// report not the statement we
+		// were looking for
+	}
+}
+
+void Parser::parse_while_statement() {
+	if (this->just_match(MP_WHILE)) {
+		this->match(MP_WHILE);
+		this->parse_boolean_expression();
+		this->match(MP_DO);
+		this->parse_statement();
+	} else {
+		// report not the statement we
+		// were looking for
+	}
+}
+
+void Parser::parse_for_statement() {
+	if (this->just_match(MP_FOR)) {
+		this->match(MP_FOR);
+		this->parse_control_variable();
+		this->match(MP_ASSIGNMENT);
+		this->parse_initial_value();
+		this->parse_step_value();
+		this->parse_final_value();
+		this->match(MP_DO);
+		this->parse_statement();
+	} else {
+		// report not the statement we
+		// were looking for
+	}
+}
+
+void Parser::parse_control_variable() {
+	this->parse_variable_identifier();
+}
+
+void Parser::parse_initial_value() {
+	this->parse_ordinal_expression();
+}
+
+void Parser::parse_step_value() {
+	if (this->just_match(MP_TO)) {
+		this->match(MP_TO);
+	} else if (this->just_match(MP_DOWNTO)) {
+		this->match(MP_DOWNTO);
+	} else {
+		// report some syntax error
+	}
+}
+
+void Parser::parse_final_value() {
+	this->parse_ordinal_expression();
+}
+
+void Parser::parse_procedure_statement() {
+	this->parse_procedure_identifier();
+	this->parse_optional_actual_parameter_list();
+}
+
+void Parser::parse_optional_actual_parameter_list() {
+	if (this->just_match(MP_LEFT_PAREN)) {
+		// optional list used
+		this->match(MP_LEFT_PAREN);
+		this->parse_actual_parameter();
+		this->parse_actual_parameter_tail();
+	} else {
+		// epsilon
+	}
+}
+
+void Parser::parse_actual_parameter_tail() {
+	if (this->just_match(MP_COMMA)) {
+		// param tail used
+		this->parse_actual_parameter();
+		this->parse_actual_parameter_tail();
+	} else {
+		// epsilon used
+	}
+}
+
+void Parser::parse_actual_parameter() {
+	this->parse_ordinal_expression();
+}
+
+void Parser::parse_expression() {
+	this->parse_simple_expression();
+	this->parse_optional_relational_part();
+}
+
+void Parser::parse_optional_relational_part() {
+	this->parse_relational_operator();
+	this->parse_simple_expression();
+}
+
+void Parser::report_error(string error) {
+	cout << "Parse Error: " << error << endl;
+}
+
+void Parser::report_match(string match) {
+	cout << match << endl;
+}
+
+void Parser::next_token() {
+	// consume the token and get the next
+	if (this->fromList == false) {
+		// get the next token from the dispatcher
+		this->lookahead = this->scanner->scan_one();
+	}
+	else if (this->fromList == true) {
+		// implement this later... with detaching token list
 	}
 }
 
--- src/Parser.hpp
+++ src/Parser.hpp
@@ -21,13 +21,94 @@ public:
 	Parser(shared_ptr<vector<shared_ptr<Token>>> token_list);
 	Parser(shared_ptr<Scanner> scanner);
 	void parseMe();
-	void match(TokType token_type);
+	void match(TokType expected);
+	bool just_match(TokType expected)
 	virtual ~Parser() {
 		if (scanner != nullptr)
 			scanner.reset();
 		if (token_list != nullptr)
 			token_list.reset();
 	}
+	// parse for all Mikropascal non-terminals
+	void parse_system_goal();
+	void parse_program();
+	void parse_program_heading();
+	// the program basically
+	void parse_block();
+	// variable declaration stuff
+	void parse_variable_declaration_part();
+	void parse_variable_declaration_tail();
+	void parse_variable_declaration();
+	void parse_type();
+	// procedure and function declaration stuff
+	void parse_procedure_and_function_declaration_part();
+	void parse_procedure_declaration();
+	void parse_function_declaration();
+	void parse_procedure_heading();
+	void parse_function_heading();
+	void parse_optional_formal_parameter_list();
+	void parse_formal_parameter_section_tail();
+	void parse_formal_parameter_section();
+	void parse_value_parameter_section();
+	void parse_variable_parameter_section();
+	// parse statements
+	void parse_statement_part();
+	void parse_compound_statement();
+	void parse_statement_sequence();
+	void parse_statement_tail();
+	void parse_statement();
+	void parse_empty_statement();
+	void parse_read_statement();
+	void parse_read_parameter_tail();
+	void parse_read_parameter();
+	void parse_write_statement();
+	void parse_write_parameter_tail();
+	void parse_write_parameter();
+	void parse_assignment_statement();
+	// parse control flow statements
+	void parse_if_statement();
+	void parse_optional_else_part();
+	// parse looping statements
+	void parse_repeat_statement();
+	void parse_while_statement();
+	void parse_for_statement();
+	void parse_control_variable();
+	void parse_initial_value();
+	void parse_step_value();
+	void parse_final_value();
+	// parse procedure statements
+	void parse_procedure_statement();
+	void parse_optional_actual_parameter_list();
+	void parse_actual_parameter_tail();
+	void parse_actual_parameter();
+	// parse expressions
+	void parse_expression();
+	void parse_optional_relational_part();
+	void parse_relational_operator();
+	void parse_simple_expression();
+	void parse_term_tail();
+	void parse_optional_sign();
+	void parse_adding_operator();
+	void parse_term();
+	void parse_factor_tail();
+	void parse_multiplying_operator();
+	void parse_factor();
+	// parse identifiers
+	void parse_program_identifier();
+	void parse_variable_identifier();
+	void parse_procedure_identifier();
+	void parse_function_identifier();
+	void parse_boolean_expression();
+	void parse_ordinal_expression();
+	void parse_identifier_list();
+	void parse_identifier_tail();
+	// parse end of file
+	void parse_eof();
+
+	// error reporting
+	void report_error(string error);
+	void report_match(string match);
+	void next_token();
 };
 
 
